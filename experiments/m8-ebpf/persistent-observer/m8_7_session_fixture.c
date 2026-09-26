d17fe98981bf21b8fb7d70ed531e98c08f18917b
AHMED YOUNIS <aether.x.eg@gmail.com>
2026-09-26 21:15:02 +0300

    M8.7a: move root barrier after bootstrap exec

--- experiments/m8-ebpf/persistent-observer/m8_7_session_fixture.c
+++ experiments/m8-ebpf/persistent-observer/m8_7_session_fixture.c
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: Apache-2.0
 #include <errno.h>
+#include <limits.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
@@ -30,11 +32,42 @@ static int run_tree(void)
     return 0;
 }
 
+static int run_barrier(const char *self, const char *fd_text)
+{
+    char *end = NULL;
+    errno = 0;
+    long parsed = strtol(fd_text, &end, 10);
+    if (errno != 0 || end == fd_text || *end != '\0' || parsed < 0 || parsed > INT_MAX) {
+        fprintf(stderr, "invalid barrier fd\n");
+        return 23;
+    }
+
+    int fd = (int)parsed;
+    unsigned char byte = 0;
+    ssize_t rc;
+    do {
+        rc = read(fd, &byte, 1);
+    } while (rc < 0 && errno == EINTR);
+    close(fd);
+
+    if (rc != 1) {
+        fprintf(stderr, "root-registration barrier release failed\n");
+        return 24;
+    }
+
+    execl(self, self, "tree", (char *)NULL);
+    perror("exec self after root-registration barrier");
+    return 25;
+}
+
 int main(int argc, char **argv)
 {
-    if (argc != 2 || strcmp(argv[1], "tree") != 0) {
-        fprintf(stderr, "usage: %s tree\n", argv[0]);
-        return 2;
-    }
-    return run_tree();
+    if (argc == 2 && strcmp(argv[1], "tree") == 0)
+        return run_tree();
+
+    if (argc == 3 && strcmp(argv[1], "barrier") == 0)
+        return run_barrier(argv[0], argv[2]);
+
+    fprintf(stderr, "usage: %s tree | barrier FD\n", argv[0]);
+    return 2;
 }
