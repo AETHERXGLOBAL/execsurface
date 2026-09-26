5d637ca9d0bd4626007947bd8cd10ae0a154b517
AHMED YOUNIS <aether.x.eg@gmail.com>
2026-09-26 16:13:04 +0300

    M8.3b: add libbpf skeleton build

---
+++ experiments/m8-ebpf/libbpf-path-bridge/build.rs
@@ -0,0 +1,18 @@
+use std::ffi::OsStr;
+use std::path::PathBuf;
+
+fn main() {
+    let out = PathBuf::from(std::env::var_os("OUT_DIR").expect("OUT_DIR"))
+        .join("probe.skel.rs");
+
+    libbpf_cargo::SkeletonBuilder::new()
+        .source("src/bpf/probe.bpf.c")
+        .clang_args([
+            OsStr::new("-I"),
+            OsStr::new("/usr/include/x86_64-linux-gnu"),
+        ])
+        .build_and_generate(&out)
+        .expect("build and generate libbpf skeleton");
+
+    println!("cargo:rerun-if-changed=src/bpf/probe.bpf.c");
+}
