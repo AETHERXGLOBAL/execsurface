e4d8b7addf1c39f4a8aa36054712e76040420c80
AHMED YOUNIS <aether.x.eg@gmail.com>
2026-09-26 20:48:58 +0300

    M8.7a: apply Rust 1.82 formatting

--- experiments/m8-ebpf/persistent-observer/build.rs
+++ experiments/m8-ebpf/persistent-observer/build.rs
@@ -2,8 +2,8 @@ use std::ffi::OsStr;
 use std::path::PathBuf;
 
 fn main() {
-    let out = PathBuf::from(std::env::var_os("OUT_DIR").expect("OUT_DIR"))
-        .join("persistent.skel.rs");
+    let out =
+        PathBuf::from(std::env::var_os("OUT_DIR").expect("OUT_DIR")).join("persistent.skel.rs");
 
     libbpf_cargo::SkeletonBuilder::new()
         .source("src/bpf/persistent.bpf.c")
