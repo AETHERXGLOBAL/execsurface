c2efc7487bd2e455dc56a521fe159648554768b8
AHMED YOUNIS <aether.x.eg@gmail.com>
2026-09-26 21:54:17 +0300

    M8.7b5: gate tiny ring-buffer fault build

--- experiments/m8-ebpf/persistent-observer/build.rs
+++ experiments/m8-ebpf/persistent-observer/build.rs
@@ -12,6 +12,9 @@ fn main() {
     if std::env::var_os("M8_7_FAULT_SMALL_TASK_MAP").is_some() {
         clang_args.push(OsStr::new("-DM8_7_TASK_EPOCH_MAX_ENTRIES=8"));
     }
+    if std::env::var_os("M8_7_FAULT_SMALL_RINGBUF").is_some() {
+        clang_args.push(OsStr::new("-DM8_7_RINGBUF_MAX_ENTRIES=4096"));
+    }
 
     libbpf_cargo::SkeletonBuilder::new()
         .source("src/bpf/persistent.bpf.c")
@@ -21,4 +24,5 @@ fn main() {
 
     println!("cargo:rerun-if-changed=src/bpf/persistent.bpf.c");
     println!("cargo:rerun-if-env-changed=M8_7_FAULT_SMALL_TASK_MAP");
+    println!("cargo:rerun-if-env-changed=M8_7_FAULT_SMALL_RINGBUF");
 }
